39440c486ce75924da7753b188497e20aabc9fe7
Damian Norman <damiannorman@gmail.com>
2020-11-10 00:49:09 -0500

    Add files via upload

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+const fetchWeather = async (cityName = 'new york') => {
+  const API_KEY = "<INSERT_API_KEY>";
+  // TODO: fetch weather forecast from endpoint
+  // from https://openweathermap.org/api
+};
